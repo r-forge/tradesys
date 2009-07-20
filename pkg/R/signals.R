764c05f1f34b073258013d784b635e0dccb18a69
rmsams (Mr. ) <robert@sanctumfi.com>
2009-07-20 17:09:33 +0000

    Debug entrycond=TRUE error in signals.. modify symantics of binary signals 12-15.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tradesys@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/signals.R
+++ pkg/R/signals.R
@@ -1,16 +1,25 @@
 signals <- function(el=FALSE, es=FALSE, xl=FALSE, xs=FALSE, entrycond=FALSE, entrywins=FALSE){
+  ## NA's are treated as FALSE
   el[which(is.na(el))] <- FALSE
   es[which(is.na(es))] <- FALSE
   xl[which(is.na(xl))] <- FALSE
   xs[which(is.na(xs))] <- FALSE
   if(entrycond){
     if(any(xl)){
-      for(i in which(xl))
-        el[i:min(which(j <- which(es) > i)[j])] <- FALSE
+      for(i in which(xl)){
+        if(any(j <- es & c(rep(FALSE, i), rep(TRUE, length(es) - i))))
+          el[i:min(which(j))] <- FALSE
+        else
+          el[i:length(el)] <- FALSE
+      }
     }
     if(any(xs)){
-      for(i in which(xs))
-        es[i:min(which(j <- which(el) > i)[j])] <- FALSE
+      for(i in which(xs)){
+        if(any(j <- el & c(rep(FALSE, i), rep(TRUE, length(el) - i))))
+          es[i:min(which(j))] <- FALSE
+        else
+          es[i:length(es)] <- FALSE
+      }
     }
   }
   l <-lapply(list(el, es, xl, xs), length)
@@ -18,14 +27,15 @@ signals <- function(el=FALSE, es=FALSE, xl=FALSE, xs=FALSE, entrycond=FALSE, ent
     stop("el, es, xl, and xs must all be the same length.")
   x <- cbind(el, es, xl, xs)
   x <- apply(matrix(x, ncol=4), 2, function(x){x[which(is.na(x))] <- 0; x})
-  PHASES <- c("UC","XS","XL","XA","ES","XS","ES","XA","EL","EL","XL","XA")
+  PHASES <- c("UC","XS","XL","XA","ES","XS","ES","XA",
+              "EL","EL","XL","XA","UC","XS","XL","XA")
   if(entrywins){
     PHASES[c(6,8)] <- "ES"
     PHASES[c(11,12)] <- "EL"
   }
   y <- apply(matrix(x, ncol=4), 1, function(x){sum(c(8,4,2,1) * x)})
   if(any(y > 11))
-    stop("el and es cannot both be TRUE.")
+    message(paste("el and es cannot both be TRUE.. both are set to FALSE at:", paste(which(y), collapse=",")))
   statemap(PHASES[y+1])
 }
 
@@ -36,17 +46,18 @@ signals <- function(el=FALSE, es=FALSE, xl=FALSE, xs=FALSE, entrycond=FALSE, ent
 ## 0010 = 2  --> XL
 ## 0011 = 3  --> XA
 ## 0100 = 4  --> ES
-## 0101 = 5  --> XS*
+## 0101 = 5  --> XS-
 ## 0110 = 6  --> ES
-## 0111 = 7  --> XA*
+## 0111 = 7  --> XA-
 ## 1000 = 8  --> EL
 ## 1001 = 9  --> EL
-## 1010 = 10 --> XL*
-## 1011 = 11 --> XA*
-## 1100 = 12 --> --
-## 1101 = 13 --> --
-## 1110 = 14 --> --
-## 1111 = 15 --> --
+## 1010 = 10 --> XL+
+## 1011 = 11 --> XA+
+## 1100 = 12 --> UC*
+## 1101 = 13 --> XS*
+## 1110 = 14 --> XL*
+## 1111 = 15 --> XA*
 ## ----------------
-## * EL or ES if !entrywins 
-
+## - ES if entrywins
+## + EL if entrywins
+## * Notice raised
