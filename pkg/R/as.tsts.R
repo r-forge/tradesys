f50392880933a44c4c6670edc09e05eada1190a3
rmsams (Mr. ) <robert@sanctumfi.com>
2009-07-22 10:57:30 +0000

    Removed as.tsts generic and default method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tradesys@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/as.tsts.R
+++
@@ -1,7 +0,0 @@
-as.tsts <- function(x, ...){
-  UseMethod("as.tsts")
-}
-
-as.tsts.default <- function(x, order.by=index(x), states=NULL, roll.at=NULL, pricecols=1, ...){
-  tsts(x, order.by, states, roll.at, pricecols)
-}
