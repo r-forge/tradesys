10f85a82321f15ccbca37070324584dcd535db2f
rmsams (Mr. ) <robert@sanctumfi.com>
2009-07-22 11:31:57 +0000

    Add states<-() and phases<-()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tradesys@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/phases.R
+++ pkg/R/phases.R
@@ -1,3 +1,8 @@
 phases <- function(x){
   phasemap(states(x))
 }
+
+"phases<-" <- function(x, value){
+  states(x) <- statemap(value)
+  x
+}
--- pkg/R/states.R
+++ pkg/R/states.R
@@ -1,7 +1,18 @@
 states <- function(x){
-  if(!is.tsts(x))
-    stop("x must be class 'tsts'")
+  if(!"St" %in% colnames(x))
+    stop("x doesn't contain a states column 'St'.")
   y <- x[, "St"]
   names(y) <- NULL
   y
+}
+
+"states<-" <- function(x, value){
+  if(!"St" %in% colnames(x))
+    stop("x doesn't contain a states column 'St'.")
+  if(!any(value %in% c(1,0,-1)))
+    stop("value must be a numeric vector containing only 1, 0, and -1 values.")
+  if(nrow(x) %% length(value) != 0)
+    stop("length(value) must be a multiple of nrow(x).")
+  x[, "St"] <- cbind(x[, "St"], value)[, 2]
+  x
 }
