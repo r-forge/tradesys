3ea2ccb44401234596e9d9388df824dc9204ad47
rmsams (Mr. ) <robert@sanctumfi.com>
2009-08-05 23:39:43 +0000

    Finished tsys class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tradesys@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/tsys.R
+++ pkg/R/tsys.R
@@ -1,8 +1,45 @@
-tradesys <- function(datacols, pricecols, el=FALSE, es=FALSE, xl=FALSE, xs=FALSE, 
-                    delta=1, size.at=as.logical(c(St[1], diff(St))),
-                    roll.at=FALSE, formulae=NULL, percent=TRUE, entrywins=FALSE,
-                    entrycond=FALSE){
-  l <- list(pricecols=pricecols)
+tradesys <- function(datacols, pricecols=datacols[1], el=FALSE, es=FALSE, 
+                     xl=FALSE, xs=FALSE, delta=1, size.at=as.logical(c(St[1], diff(St))),
+                     roll.at=FALSE, formulae=NULL, percent=TRUE, entrywins=FALSE,
+                     entrycond=FALSE){
+  l <- list()
+  ## Process datacols
+  if(any(duplicated(datacols)))
+    stop("datacols be unique.")
+  l$datacols <- datacols
+  ## Process pricecols arg
+  if(!is.vector(pricecols) | !is.character(pricecols))
+    stop("pricecols must be a character vector of column names.")
+  pricecols <- pricecols[1:min(length(pricecols), 5)]
+  if(length(pricecols) == 1) ## everyone gets what's passed
+    names(pricecols) <- NULL
+  if(is.null(names(pricecols))) ## order determines mapping
+    names(pricecols) <- c("Mark","Long","Short","RollLong","RollShort")[1:length(pricecols)]
+  if(any(!names(pricecols) %in% c("Mark","Long","Short","RollLong","RollShort")))
+    stop("All names(pricecols) must be among 'Mark','Long','Short','RollLong','RollShort'")
+  if(any(!pricecols %in% datacols))
+    stop("all pricecols must be in datacols or names(formulae)")
+  l$pricecols <- c(pricecols["Mark"], pricecols["Long"], pricecols["Short"], pricecols["RollLong"], pricecols["RollShort"])
+  names(l$pricecols) <- c("Mark","Long","Short","RollLong","RollShort")
+  if(is.na(l$pricecols["Mark"]))
+    stop("pricecols must be passed a value for 'Mark'")
+  if(is.na(l$pricecols["Long"]))
+    l$pricecols["Long"] <- l$pricecols["Mark"]
+  if(is.na(l$pricecols["Short"]))
+    l$pricecols["Short"] <- l$pricecols["Mark"]
+  if(is.na(l$pricecols["RollShort"]))
+    l$pricecols["RollShort"] <- l$pricecols["Short"]
+  if(is.na(l$pricecols["RollLong"]))
+    l$pricecols["RollLong"] <- l$pricecols["Long"]
+  ## Process formulae arg (we expect a named list of calls)
+  if(!is.null(formulae)){
+    if(!is.list(formulae) | is.null(names(formulae)) | any(duplicated(names(formulae))) | any(names(formulae) %in% datacols))
+      stop("formulae must be a list with names that are unique and not in datacols")
+    if(any(unlist(lapply(formulae, class)) != "call"))
+      stop("formulae must be a list of call objects")
+    l$formulae <- formulae
+  }
+  ## Process expression args
   l$el <- substitute(el)
   l$es <- substitute(es)
   l$xl <- substitute(xl)
@@ -10,12 +47,74 @@ tradesys <- function(datacols, pricecols, el=FALSE, es=FALSE, xl=FALSE, xs=FALSE
   l$delta <- substitute(delta)
   l$size.at <- substitute(size.at)
   l$roll.at <- substitute(roll.at)
-  l$formulae <- substitute(formulae)
+  ## Process logical args
+  if(!is.logical(percent))
+    stop("percent must be logical")
+  if(!is.logical(entrywins))
+    stop("entrywins must be logical")
+  if(!is.logical(entrycond))
+    stop("entrycond must be logical")
   l$percent <- percent
   l$entrywins <- entrywins
   l$entrycond <- entrycond
+  class(l) <- "tsys"
   l
 }
+
+##
+## Solver
+##
+
+solvets <- function(x, data, order.by=index(data)){
+  if(length(order.by) != length(unique(order.by)))
+    stop("all order.by must be unique.")
+  data <- as.matrix(data)
+  rownames(data) <- NULL
+  ## Evaluate formulae
+  data <- cbind(data, do.call("cbind", lapply(x$formulae, eval, as.list(as.data.frame(data)))))
+  ## Evaluate signals
+  Frame <- as.list(as.data.frame(data))
+  Frame$index <- order.by
+  St <- do.call("signalmap", lapply(c(x["el"], x["es"], x["xl"], x["xs"], x["entrycond"], x["entrywins"]), eval, Frame))
+  St <- cbind(St, data)[, 1]
+  ## Evaluate delta, roll.at, size.at
+  Frame <- as.list(as.data.frame(data))
+  Frame$St <- St
+  Frame$index <- order.by
+  delta <- cbind(eval(x$delta, Frame), data)[, 1]
+  size.at <- cbind(eval(x$size.at, Frame), data)[, 1]
+  roll.at <- cbind(eval(x$roll.at, Frame), data)[, 1]
+  ## Calculate equity
+  Equity <- equity(prices(cbind(data, St), x$pricecols), St, delta, size.at, roll.at, x$percent)[, "Equity"]
+  list(St=St, Equity=Equity, delta=delta, size.at=size.at, roll.at=roll.at)
+}
+
+##
+## Methods
+##
+
+print.tsys <- function(x, ...){
+  for(i in 1:length(x))
+    cat(names(x)[i],":", format(x[[i]]), "\n")
+}
+
+"$<-.tsys" <- function(x, name, value){
+  y <- x
+  y[[name]] <- value
+  y <- try(do.call("tradesys", y))
+  if(class(y) == "try-error")
+    return(x)
+  y
+}
+
+as.list.tsys <- function(x, ...){
+  class(x) <- NULL
+  x
+}
+
+##
+## Extractors
+##
 
 tsys <- function(x){
   if(!is.tsts(x))
--- pkg/readme.txt
+++ pkg/readme.txt
@@ -1,10 +1,16 @@
 Todo
 
-- Code limited class-preserving subsetting to [ and code its
+- Incorporate signals into return of solvets.
+- Let solvets take 'tsts' objects.. or make it a generic?
+- Remove tstsp.
+- Write tsys.Rd and rewrite tsts.R, tsts.Rd to incorporate tsys class.
+- Remove convenience functions and document states and prices seperately.
+- Code limited class-preserving subsetting to [.tsts and code its
   assingment method to enforce 'formulae' restrictions.
 - Fix trades.
 - Fix states (assignment method should calc signals).
-- Write first-draft of summary.tsts.
 - Optimize equity by vectorizing all calculations.
+- Improve print.tsys.
+- Write summary.tsts.
 
 
