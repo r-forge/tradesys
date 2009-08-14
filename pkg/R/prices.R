388924717399ccb742e17baba63e492e06edf011
rmsams (Mr. ) <robert@sanctumfi.com>
2009-08-14 18:52:08 +0000

    Hacked prices and added rollTest.R to the unit tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tradesys@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/prices.R
+++ pkg/R/prices.R
@@ -1,4 +1,4 @@
-prices <- function(x, states, pricemap){
+prices <- function(x, states, pricemap, roll.at=FALSE){
   ## process x
   if(is.tsts(x)){
     states <- x[, "St"]
@@ -14,6 +14,8 @@ prices <- function(x, states, pricemap){
   if(nrow(x) %% length(states) != 0)
     stop("length(states) must be a multiple of nrow(x)")
   states <- cbind(states, x)[, 1]
+  ## process roll.at
+  roll.at <- as.logical(cbind(roll.at, x)[, 1])
   if(any(is.na(y[, "Mark"]))){ ## fill NA's in Mark column
     message("NA's in 'Mark' column.. filling with previous value")
     y[, "Mark"] <- na.locf(y[, "Mark"])
@@ -22,15 +24,17 @@ prices <- function(x, states, pricemap){
   for(i in colnames(y)){ ## ...other cols with NA's get Mark's value
     if(any(n <- is.na(y[, i]))){
       message(paste("NA's in", i, "column.. filling with value in 'Mark' column."))
-      y[which(n), i] <- y[which(n), "Mark"]
+      y[n, i] <- y[n, "Mark"]
     }
   }
   y <- cbind(Price=y[, "Mark"], Roll=y[, "RollLong"], y)
   h <- phasemap(states)
-  y[which(h == "EL"), "Price"] <- y[which(h == "EL"), "Long"]
-  y[which(h == "ES"), "Price"] <- y[which(h == "ES"), "Short"]
-  y[which(h == "XL"), "Price"] <- y[which(h == "XL"), "Short"]
-  y[which(h == "XS"), "Price"] <- y[which(h == "XS"), "Long"]
-  y[which(states == -1), "Roll"] <- y[which(states == -1), "RollShort"]
+  y[h == "EL", "Price"] <- y[h == "EL", "Long"]
+  y[h == "ES", "Price"] <- y[h == "ES", "Short"]
+  y[h == "XL", "Price"] <- y[h == "XL", "Short"]
+  y[h == "XS", "Price"] <- y[h == "XS", "Long"]
+  y[roll.at & states == 1, "Price"] <-   y[roll.at & states == 1, "Short"]
+  y[roll.at & states == -1, "Price"] <-   y[roll.at & states == -1, "Long"] 
+  y[states == -1, "Roll"] <- y[states == -1, "RollShort"]
   y
 }
---
+++ pkg/inst/tests/rollTest.R
@@ -0,0 +1,58 @@
+test.roll <- function(){
+
+  ## These are the closing Bid/Ask/Close prices for the second and third Eurodollar
+  ## series. The contract rolled on 06/15, so ED2 represents EDU9 until 06/16, at
+  ## which time it starts representing EDZ9. (Similarly, ED3 represents EDZ9 before
+  ## and EDH0 after.) 
+
+  ##              ED2.BID ED2.ASK ED2.CLS ED3.BID ED3.ASK ED3.CLS
+  ## 1 2009-06-10  99.190  99.220  99.210  98.770  98.795  98.790
+  ## 2 2009-06-11  99.230  99.250  99.240  98.835  98.880  98.850
+  ## 3 2009-06-12  99.235  99.275  99.265  98.880  98.925  98.900
+  ## 4 2009-06-15  99.245  99.285  99.265  98.930  98.945  98.930
+  ## 5 2009-06-16  98.950  98.955  98.935  98.660  98.670  98.635
+  ## 6 2009-06-17  98.940  98.960  98.945  98.660  98.670  98.670
+  ## 7 2009-06-18  98.820  98.860  98.840  98.450  98.600  98.525
+  ## 8 2009-06-19  98.935  98.965  98.935  98.640  98.680  98.630
+
+  ed <- cbind(c(99.190,99.230,99.235,99.245,98.950,98.940,98.820,98.935),
+              c(99.220,99.250,99.275,99.285,98.955,98.960,98.860,98.965),
+              c(99.210,99.240,99.265,99.265,98.935,98.945,98.840,98.935),
+              c(98.770,98.835,98.880,98.930,98.660,98.660,98.450,98.640),
+              c(98.795,98.880,98.925,98.945,98.670,98.670,98.600,98.680),
+              c(98.790,98.850,98.900,98.930,98.635,98.670,98.525,98.630))
+
+  colnames(ed) <- c("ED2.BID","ED2.ASK","ED2.CLS","ED3.BID","ED3.ASK","ED3.CLS")
+
+  ## The roll is at row 4
+
+  roll.at <- c(FALSE,FALSE,FALSE,TRUE,FALSE,FALSE,FALSE,FALSE)
+
+  ## The pricemap is:
+  
+  ## Mark:      ED2.CLS
+  ## Long:	ED2.ASK
+  ## Short:	ED2.BID
+  ## RollLong:  ED3.ASK
+  ## RollShort: ED3.BID
+  
+  prcmap <- c(Mark="ED2.CLS",Long="ED2.ASK",Short="ED2.BID",RollLong="ED3.ASK",RollShort="ED3.BID")
+
+  ## prices()
+  
+  ## Case #1: Long at roll time 
+  checkEquals(prices(ed, 1, prcmap, roll.at)[4, "Price"], 99.245, checkNames=FALSE) ## Price[4] = 99.245 (Short price). 
+  checkEquals(prices(ed, 1, prcmap, roll.at)[4, "Roll"], 98.945, checkNames=FALSE)  ## Price[4] = 98.945 (RollLong price). 
+  ## Case #2: Short at roll time 
+  checkEquals(prices(ed, -1, prcmap, roll.at)[4, "Price"], 99.285, checkNames=FALSE)## Price[4] = 99.285 (Long price). 
+  checkEquals(prices(ed, -1, prcmap, roll.at)[4, "Roll"], 98.930, checkNames=FALSE) ## Price[4] = 98.930 (RollShort price). 
+  ## Case #3: Flat at roll time 
+  checkEquals(prices(ed, 0, prcmap, roll.at)[4, "Price"], 99.265, checkNames=FALSE) ## Price[4] = 99.265 (Mark price). 
+  checkEquals(prices(ed, 0, prcmap, roll.at)[4, "Roll"], 98.945, checkNames=FALSE)  ## Price[4] = 98.945 (RollLong price).
+
+  ## equity
+  ## equity(prices(ed, 1, prcmap, roll.at), states=1, roll.at=roll.at, percent=FALSE)
+
+  ## trades
+  ## trades(prices(ed, 1, prcmap, roll.at), states=1, roll.at=roll.at, percent=FALSE)
+}
